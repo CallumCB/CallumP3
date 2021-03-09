156a8a0c04af5a398f78ff4cae893a5214eae9c9
CallumCB <77706780+CallumCB@users.noreply.github.com>
2021-03-09 12:03:55 -0700

    Add files via upload

---
+++ codeAlongP3.py
@@ -0,0 +1,25 @@
+"""
+Program Goals
+Get the input
+Convert inout to INT
+Add input to a list
+Pull values from specific index positions
+
+"""
+
+def mainProgram():
+    myList=[]
+    print("Hi")
+    print("Choose one of the following options, Type a number only.")
+    choice = input("1. Add to list, 2.Return the value at and index position   ")
+    if choice == "1":
+        addToList()
+    elif choice == "2":
+        indexValues
+
+def addToList():
+
+def indexValues():
+
+if __name__ == "__main__":
+    mainProgram()
