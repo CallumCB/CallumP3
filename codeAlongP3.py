55bc9d4b7e438cf91e850728b2a43381ef34a3b5
CallumCB <77706780+CallumCB@users.noreply.github.com>
2021-03-12 11:57:48 -0700

    Update codeAlongP3.py

--- codeAlongP3.py
+++ codeAlongP3.py
@@ -4,22 +4,24 @@ import random
 myList = []
 def mainProgram():
     while True:
-        print("Choose one of the following options. type a number only.")
-        choice = input("""1. Add to list,
-2. Return the value at an index position
-3. End
-4. Pick random number from list.
-""")
-        if choice == "1":
-            addToList()
-        elif choice == "2":
-            indexValues()
-        elif choice=="3":
-            break
-        elif choice=='4':
-            randomSearch()
-    #need to add way to exit the program and catch errors made by the user
-
+        try:
+            print("Choose one of the following options. type a number only.")
+            choice = input("""1. Add to list,
+    2. Return the value at an index position
+    3. Pick random number from list
+    4. End
+    """)
+            if choice == "1":
+                addToList()
+            elif choice == "2":
+                indexValues()
+            elif choice=="3":
+                randomSearch()
+            else:
+                break
+        except:
+            print("There was an error.")
+        
 def addToList():
     newItem=input("Please type an integer.  ")
     myList.append(int(newItem))
