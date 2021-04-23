c8a02226b94b8518ead8b1652d55b1e57477ed84
CallumCB <77706780+CallumCB@users.noreply.github.com>
2021-04-23 11:35:53 -0600

    Update codeAlongP3.py

--- codeAlongP3.py
+++ codeAlongP3.py
@@ -45,10 +45,17 @@ def mainProgram():
                 break
         except:
             print("There was an error.")
+
+""" This functions asks what you want to add to the list, it then takes that and
+turns it into an integer, it then adds it into the latest position of the list."""
 def addToList():
     newItem=input("Please type an integer.  ")
     myList.append(int(newItem))
     print(myList)
+    
+""" This does the same as the add function, but it gives you a range that you can choose
+how high you want the numbers to go, and then you can choose how many numbers you want to add
+It then adds it to the end of the list"""
 def addLot():
     print("About to add lot's"   )
     numToAdd=input("How many integers do you want to add?   ")
@@ -56,6 +63,10 @@ def addLot():
     for x in range(0, int(numToAdd)):
         myList.append(random.randint(0, int(numRange)))
     print("Your list is done.")
+    
+""" What this function does, is it takes the first number, sees if it is in the other list, if it is not, then it adds it to the list
+It goes to the next number and does it again until there are no numbers left. At the end of adding the new numbers, there is a sort, wich will
+sort all of the numbers from lowest to highest in the new list."""
 def sortList(myList):
     for x in myList:
         if x not in unique_list:
@@ -64,21 +75,32 @@ def sortList(myList):
     showMe=input("Want to see your new list?  Y/N  ")
     if showMe.lower() == 'y':
         print(unique_list)
+        
+""" This asks if you want to see the list, if so, then it takes your input index position and finds it in the list, then prints it to you. """
 def indexValues():
     indexPos = input("What position do you want to see?  ")
     print(myList[int(indexPos)])
+    
+""" This function generates a random number, and then finds that index position, then prints it """
 def randomSearch():
     print("Here is a random number from your list.")
     print(myList[random.randint(0, len(myList)-1)])
+    
+""" This functions sees if there is anything in the new list, if you have not sorted it yet, there will be nothing there, so it will
+print the original list, if there is stuff in it, it will ask wich one you want to see, then print that"""
 def printLists():
     if len(unique_list)==0:
         print(myList)
     else:
-        whichOne=input("Which list? Sorted or un-sorted?")
+        whichOne=input("Which list? sorted or un-sorted?")
         if whichOne.lower()=="sorted":
             print(unique_list)
         else:
             print(myList)
+            
+""" This asks if you what number you want to search for, it then looks at the first number
+if it is the number, then it will add 1 to how many times it is inthe list, it will then
+look at the next number and do the same untl the end of the list, then it prints how many times it was found."""
 def linearSearch():
     print("We're going to search the list")
     searchItem=input("What are you looking for?  ")
@@ -89,6 +111,9 @@ def linearSearch():
             print("Your number is at index {}".format(x))
     print("The number {}".format(searchItem))
     print("appeared {} times".format(numAm))
+    
+""" Recursive search looks in the middle of the list, if it your number, then it prints that,
+if not, then it looks at the numbers next to it, if those arent it, then they go to another position and repeats until it finds the number."""
 def recursiveBinarySearch(unique_list, low, high, x):
     if high>=low:
         mid=(high+low) // 2
@@ -100,7 +125,9 @@ def recursiveBinarySearch(unique_list, low, high, x):
         else:
             return recursiveBinarySearch(unique_list, mid + 1, high, x)
     else:
-        print("That number isn't in this list.") 
+        print("That number isn't in this list.")
+""" I don't really know how this one works, I am pretty sure that it sorts the list and removes all the numbers that
+repeat, that is because it doesn't have a print or input, so I don't really know what it does. """
 def interativeBinarySearch(unique_list, x):
     low=0
     high=len(unique_list)-1
